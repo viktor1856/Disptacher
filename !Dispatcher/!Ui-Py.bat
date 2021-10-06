574024416febafaa8a09bcaa909d006f16ec4b37
Viktor.Vtorushin <v.s.vtorushin@gmail.com>
2021-10-06 15:36:51 +0300

    Изменена главная форма, отрисована форма заказчика транспорта

--- !Dispatcher/!Ui-Py.bat
+++ !Dispatcher/!Ui-Py.bat
@@ -3,3 +3,4 @@ pyuic5 -x newAutoForm.ui -o newAutoForm.py
 pyuic5 -x newDriverForm.ui -o newDriverForm.py
 pyuic5 -x listDriverForm.ui -o listDriverForm.py
 pyuic5 -x listAutoForm.ui -o listAutoForm.py
+pyuic5 -x customerListForm.ui -o customerListForm.py
